84b619edc96fb7d1d05079985d003e7ad3a8b4ad
Jahid <s.jahid.90@gmail.com>
2020-02-02 15:26:09 +0530

    [client] remove unused code; convert app to hello world

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders hello world', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const text = getByText(/Hello World!/i);
+  expect(text).toBeInTheDocument();
 });
