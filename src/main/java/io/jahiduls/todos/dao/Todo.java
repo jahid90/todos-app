21e453a460ccc80feb79506427e025ac1a413c50
Jahid <s.jahid.90@gmail.com>
2020-07-11 22:34:18 +0530

    [service] add toString method to todo

--- src/main/java/io/jahiduls/todos/dao/Todo.java
+++ src/main/java/io/jahiduls/todos/dao/Todo.java
@@ -2,10 +2,12 @@ package io.jahiduls.todos.dao;
 
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document
+@ToString
 @NoArgsConstructor
 @AllArgsConstructor
 public class Todo {
