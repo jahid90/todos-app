7a62e6af06622c69e7ccf3fba14907630844497d
Jahid <s.jahid.90@gmail.com>
2020-07-11 14:58:33 +0530

    annotate todo as mongo document

--- src/main/java/io/jahiduls/todos/dao/Todo.java
+++ src/main/java/io/jahiduls/todos/dao/Todo.java
@@ -2,14 +2,13 @@ package io.jahiduls.todos.dao;
 
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
-import lombok.ToString;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
-@ToString
+@Document
 @NoArgsConstructor
 @AllArgsConstructor
 public class Todo {
-
     @Id public String id;
     public String text;
     public boolean isCompleted;
