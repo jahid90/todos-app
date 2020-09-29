b3e5f9caf5fedee4ac43070432e81c36e469aab9
Jahid <s.jahid.90@gmail.com>
2020-09-29 19:28:33 +0530

    [client] fix bad test; remnant from package initialization

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders hello world', () => {
+test('renders all todos', () => {
   const { getByText } = render(<App />);
-  const text = getByText(/Hello World!/i);
+  const text = getByText(/All Todos/i);
   expect(text).toBeInTheDocument();
 });
