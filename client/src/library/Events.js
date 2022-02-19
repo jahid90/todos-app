aa6d9762776b1b135ee99bc92337430af5ab2d42
Jahid <jahid90@users.noreply.github.com>
2022-02-19 18:19:12 +0530

    [client] fix eslint error; add prettier config

--- client/src/library/Events.js
+++ client/src/library/Events.js
@@ -2,8 +2,8 @@ export const Events = {
     TODOS_UPDATED: 'todos-updated',
     TODO_ADDED: 'todo-added',
     TODO_TOGGLED: 'todo-toggled',
-    TODO_DELETED: 'todo-deleted'
-}
+    TODO_DELETED: 'todo-deleted',
+};
 
 export const Commands = {
     FETCH: 'fetch',
@@ -12,11 +12,12 @@ export const Commands = {
     DELETE_TODO: 'delete-todo',
     FILTER_ALL: 'filter-all',
     FILTER_COMPLETED: 'filter-completed',
-    FILTER_INCOMPLETE: 'filter-incomplete'
-}
+    FILTER_INCOMPLETE: 'filter-incomplete',
+};
 
-export default {
+const all = {
     Events,
     Commands,
-}
+};
 
+export default all;
